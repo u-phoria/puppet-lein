0a26bc6dad5391a9b31a6957eb5acae105cd428e
Uros Rapajic <uros.rapajic@gmail.com>
2014-03-26 01:06:16 +0000

    Update subscribe to trigger creation on lein only on latest

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,19 +4,23 @@ class lein (
    $version = 'stable',
 ) {
   package {'wget':}
+  
   file { "$tmpdir/lein-$version":
     alias => "createtmpdir",
     ensure => "directory",
   }
+  
   exec { "/usr/bin/wget https://raw.github.com/technomancy/leiningen/$version/bin/lein":
     alias => "leinlatest",
     cwd => "$tmpdir/lein-$version",
     require => File["createtmpdir"],
     creates => "$tmpdir/lein-$version/lein", # shouldn't do anything if already exists
   }
+  
   file { "$destdir/lein": 
     ensure => present,
     source => "$tmpdir/lein-$version/lein",
+    subscribe => Exec['leinlatest'],
     mode => 755,
   }
 }
